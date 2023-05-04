da172dd592cc245aadfad89b596df9d175f56a89
Camilo Uzquiano <camilo.uzquiano@outlook.com>
2023-05-04 11:11:26 -0400

    initial commit

---
+++ firebase/FirebaseConfig.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(config);
+
+const dbFirestore = getFirestore(app);
+
+const auth = getAuth();
+
+export { dbFirestore, auth };
